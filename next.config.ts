a989c96f294959aa7ce6a4a82a16eace17d08c2e
sadanandkaji <sadanandkaji1.com>
2026-04-21 17:13:02 +0530

    up4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Forwarded-Proto", value: "https" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
